05d3ba0594091e45d11567d63a3ab6e4dceb034f
Ben Gross <benrgross@gmail.com>
2021-03-08 23:35:09 -0500

    removed path.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,10 +22,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-
-app.get("/exercise", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
