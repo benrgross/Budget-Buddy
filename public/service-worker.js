a42b658613ddcca2def56981a3ea52472d07a722
Ben Gross <benrgross@gmail.com>
2021-03-09 20:52:03 -0500

    update serviceworker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,8 +20,6 @@ self.addEventListener("install", (event) => {
   );
   self.skipWaiting();
 });
-
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", (event) => {
   const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   event.waitUntil(
@@ -43,9 +41,8 @@ self.addEventListener("activate", (event) => {
       .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", (event) => {
-  // // non GET requests are not cached and requests to other origins are not cached
+  // non GET requests are not cached and requests to other origins are not cached
   if (
     event.request.method !== "GET" ||
     !event.request.url.startsWith(self.location.origin)
@@ -53,7 +50,6 @@ self.addEventListener("fetch", (event) => {
     event.respondWith(fetch(event.request));
     return;
   }
-
   // handle runtime GET requests for data from /api routes
   if (event.request.url.includes("/api/transaction")) {
     // make network request and fallback to cache if network request fails (offline)
@@ -69,19 +65,20 @@ self.addEventListener("fetch", (event) => {
     );
     return;
   }
-
   // use cache first for all other requests for performance
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return (
-        resp ||
-        fetch(event.request).then((response) => {
-          return caches.open(RUNTIME_CACHE).then((cache) => {
-            cache.put(event.request, response.clone());
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      // request is not in cache. make network request and cache the response
+      return caches.open(RUNTIME_CACHE).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
             return response;
           });
-        })
-      );
+        });
+      });
     })
   );
 });
